c307e0f413ce6ffa2e075dee8e947054d6d874dd
Frank V. Castellucci <frank.castellucci@axiom1inc.com>
2015-12-26 07:43:16 -0500

    Bump Version
    This closes #4 and closes #5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clasew "0.3.0-SNAPSHOT"
+(defproject clasew "0.3.0"
   :description "clasew: Clojure AppleScriptEngine Wrapper"
   :url "https://github.com/FrankC01/clasew"
   :license {:name "Eclipse Public License"
